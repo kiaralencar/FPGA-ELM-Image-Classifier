e4ecc75fa29b6bbc4c4fcfe5b82847f950c83119
Kiara Alencar <kiaralalencar@gmail.com>
2026-03-27 20:32:57 -0300

    Consertando o módulo de memória

--- mem_block.v
+++ mem_block.v
@@ -37,38 +37,62 @@ module mem_block (
 
 // MEM_IMG
 reg [15:0] mem_img [0:783];
-always @(posedge clk) // a escrita só acontece sincronizada com o clock
+always @(posedge clk)
     if (img_wr_en)
-        mem_img[img_addr_w] <= {8'b0, img_data_w}; // concatenação:
-assign img_data_r = mem_img[img_addr_r];
+        mem_img[img_addr_w] <= {8'b0, img_data_w};
 
-// MEM_WIN (inicializada com .mif)
-reg [15:0] mem_win [0:100351]; // declara a memória, cada posição tem 16 bits (Q4.12) e temos 100.352 posições
-initial $readmemh("W_in_q.hex", mem_win); // carrega o arquivo .hex na memória no momento que o chip "liga"
-assign win_data_r = mem_win[win_addr_r]; // quando a FSM pede um endereço, o dado aparece na saída imediatamente
+reg [15:0] img_data_r_reg;
+always @(posedge clk)
+    img_data_r_reg <= mem_img[img_addr_r];
+assign img_data_r = img_data_r_reg;
 
-// MEM_BIAS (inicializada com .mif)
+// MEM_WIN (BRAM)
+reg [15:0] mem_win [0:100351];
+initial $readmemh("W_in_q.hex", mem_win);
+
+reg [15:0] win_data_r_reg;
+always @(posedge clk)
+    win_data_r_reg <= mem_win[win_addr_r];
+assign win_data_r = win_data_r_reg;
+
+// MEM_BIAS (BRAM)
 reg [15:0] mem_bias [0:127];
 initial $readmemh("b_q.hex", mem_bias);
-assign b_data_r = mem_bias[b_addr_r];
 
-// MEM_BETA (inicializada com .mif)
+reg [15:0] b_data_r_reg;
+always @(posedge clk)
+    b_data_r_reg <= mem_bias[b_addr_r];
+assign b_data_r = b_data_r_reg;
+
+// MEM_BETA (BRAM)
 reg [15:0] mem_beta [0:1279];
 initial $readmemh("beta_q.hex", mem_beta);
-assign beta_data_r = mem_beta[beta_addr_r];
+
+reg [15:0] beta_data_r_reg;
+always @(posedge clk)
+    beta_data_r_reg <= mem_beta[beta_addr_r];
+assign beta_data_r = beta_data_r_reg;
 
 // MEM_H
 reg [15:0] mem_h [0:127];
 always @(posedge clk)
     if (h_wr_en)
         mem_h[h_addr_w] <= h_data_w;
-assign h_data_r = mem_h[h_addr_r];
+
+reg [15:0] h_data_r_reg;
+always @(posedge clk)
+    h_data_r_reg <= mem_h[h_addr_r];
+assign h_data_r = h_data_r_reg;
 
 // MEM_Y
 reg [15:0] mem_y [0:9];
 always @(posedge clk)
     if (y_wr_en)
         mem_y[y_addr_w] <= y_data_w;
-assign y_data_r = mem_y[y_addr_r];
+
+reg [15:0] y_data_r_reg;
+always @(posedge clk)
+    y_data_r_reg <= mem_y[y_addr_r];
+assign y_data_r = y_data_r_reg;
 
 endmodule
